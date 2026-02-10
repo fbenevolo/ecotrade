0bc78c0a029000fd00386a44a05a3beadcaed94e
fbenevolo <benevolofelipe@gmail.com>
2026-02-10 20:02:08 -0300

    fix on static root path

--- ecotrade/settings.py
+++ ecotrade/settings.py
@@ -126,8 +126,8 @@ LANGUAGE_CODE = 'pt-br'
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static') ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'ecotrade', 'static') ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'ecotrade', 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
